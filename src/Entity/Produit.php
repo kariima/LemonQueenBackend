50a53c31c0edeaccb134912bfacbb496f7bf3ea4
karima <karima.lafria@gmail.com>
2019-08-01 17:17:29 +0200

    ajout entity produit

---
+++ src/Entity/Produit.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $marque;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenance;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $benefice;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $composition;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $conseil;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getContenance(): ?string
+    {
+        return $this->contenance;
+    }
+
+    public function setContenance(string $contenance): self
+    {
+        $this->contenance = $contenance;
+
+        return $this;
+    }
+
+    public function getBenefice(): ?string
+    {
+        return $this->benefice;
+    }
+
+    public function setBenefice(?string $benefice): self
+    {
+        $this->benefice = $benefice;
+
+        return $this;
+    }
+
+    public function getComposition(): ?string
+    {
+        return $this->composition;
+    }
+
+    public function setComposition(?string $composition): self
+    {
+        $this->composition = $composition;
+
+        return $this;
+    }
+
+    public function getConseil(): ?string
+    {
+        return $this->conseil;
+    }
+
+    public function setConseil(?string $conseil): self
+    {
+        $this->conseil = $conseil;
+
+        return $this;
+    }
+}
